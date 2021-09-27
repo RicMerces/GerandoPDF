dfe52091be431ae05c77746e535bb384bd02af5c
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-09-27 20:46:42 -0300

    Serializer list Pessoa

--- ProjetoCompleto/GerarPDF/Program.cs
+++ ProjetoCompleto/GerarPDF/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.IO;
 
 namespace GeradorDeArquivosEmPDF
 {
     class Program
     {
+        static List<Pessoa> pessoas = new List<Pessoa>();
+
         static void Main(string[] args) { 
         
+
             Console.ReadLine();
+        }
+
+        static void DesserializarPessoas()
+        {
+            if (File.Exists("pessoas.json"))
+            {
+                using (var sr = new StreamReader("pessoas.json"))
+                {
+                    var dados = sr.ReadToEnd();
+                    pessoas = JsonSerializer.Deserialize(dados, typeof(list<Pessoa>) as List<Pessoa>);
+                }
+            }
         }
     }
 }
